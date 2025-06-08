7d9a3eaf992aff7ea6ee24b6e9f1e8a753af20f3
ZaidanZS <voynobu@gmail.com>
2025-06-09 00:24:40 +0700

    Header untuk UI fixed

---
+++ UI.h
@@ -0,0 +1,54 @@
+#ifndef UI_H
+#define UI_H
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+#include "structure.h"
+#include "fsimplement.h"
+#include "helper.h"
+#include "urimplement.h"
+#include "base.h"
+
+typedef struct {
+    WINDOW *winTree;
+    WINDOW *winDisp;
+    WINDOW *winInfo;
+    WINDOW *cur_window;
+    int maxx, maxy;
+} UIWindows;
+
+typedef struct {
+    int highlight;
+    int highlight2;
+    int tree_top;
+    int dir_top;
+    int color_offset;
+    bool IsSorted;
+} UIState;
+
+//Fungsi untuk deklarasi
+void initUI(void);
+void cleanupUI(void);
+void initWindows(UIWindows *ui);
+void resizeWindows(UIWindows *ui);
+void cleaupWindows(UIWindows *ui);
+
+//Fungsi untuk display
+void displayHeader(int color_offset);
+void DisplayTreeWindow(UIWindows *ui, char** choices, int num_choices, int highlight, int tree_top);
+void DisplayDirWindow(UIWindows *ui, char** choices, int num_choices, int highlight, int dir_top);
+void DisplayInfoWindow(UIWindows *ui, char** selected_item);
+void rainbowColor(UIWindows *ui, const char *text, int y, int x, int offset);
+
+//input handling
+int handleTreeInput(UIWindows *ui, UIState *state, int num_choices);
+int handleDirInput(UIWindows *ui, UIState *state, char **current_dir, char **sorted_dir, int current_count);
+
+//Directori operation
+void sortDirectory(char **current_dir, int current_count, char **sorted_dir);
+void freeDirectory(char **dir, int count);
+
+#endif
